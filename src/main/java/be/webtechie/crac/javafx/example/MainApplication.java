85b87b5d560bc91afaa4a6420d549b72002b9d8d
Frank Delporte <fdelporte@azul.com>
2023-11-30 16:09:24 +0100

    Clean exit of the application

--- src/main/java/be/webtechie/crac/javafx/example/MainApplication.java
+++ src/main/java/be/webtechie/crac/javafx/example/MainApplication.java
@@ -2,6 +2,7 @@ package be.webtechie.crac.javafx.example;
 
 import be.webtechie.crac.javafx.example.ui.MainWindow;
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
@@ -17,19 +18,8 @@ public class MainApplication extends Application {
 
     private static final double WIDTH = 625;
     private static final double HEIGHT = 300;
-    private final MainWindow mainWindow;
-
-    public MainApplication() {
-        mainWindow = new MainWindow();
-
-        try {
-            ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(1);
-            scheduledThreadPool.scheduleAtFixedRate(this::updateData, 1, 1, TimeUnit.SECONDS);
-            LOGGER.info("Scheduled a task to update the data");
-        } catch (Exception ex) {
-            LOGGER.error("Can't execute the task: " + ex.getMessage());
-        }
-    }
+    private final MainWindow mainWindow = new MainWindow();
+    private final ScheduledExecutorService scheduledThreadPool = Executors.newScheduledThreadPool(1);
 
     public static void main(String[] args) {
         launch();
@@ -44,9 +34,23 @@ public class MainApplication extends Application {
         stage.setTitle("JavaFX Test Application");
         stage.setScene(scene);
         stage.centerOnScreen();
+
+        // Let the application exit when the main stage is closed
+        stage.setOnCloseRequest(e -> exit());
+
         stage.show();
 
         LOGGER.info("Stage was started");
+
+        scheduledThreadPool.scheduleAtFixedRate(this::updateData, 1, 1, TimeUnit.SECONDS);
+
+        LOGGER.info("Scheduled a task to update the data");
+    }
+
+    private void exit() {
+        LOGGER.info("Stage got closed, ending the program");
+        scheduledThreadPool.shutdown();
+        Platform.exit();
     }
 
     private void updateData() {
